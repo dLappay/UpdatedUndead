aba7b851dd701a743f12587777c3504dac00dc5a
dLappay <115202489+dLappay@users.noreply.github.com>
2023-10-04 22:42:12 +0800

    Update Lich.java

--- Lich.java
+++ Lich.java
@@ -1,4 +1,6 @@
-public class Lich extends Undead{
+import java.util.Scanner;
+
+public class Lich extends Skeleton{
     static String l = "Lich";
     public Lich() {
         super();
@@ -8,13 +10,40 @@ public class Lich extends Undead{
         int getEnemyHP = (int) (enemy.getHP() * 0.10);
         setHp(super.getHP() + getEnemyHP);
         enemy.setHp(enemy.getHP() - getEnemyHP);
+        System.out.println(super.getName() + " get additional " + super.getHP() + " HP");
     }
     public void attack(Undead enemy) {
-        int attackDamage = (int) (super.getHP() * 0.70);
-        enemy.setHp(enemy.getHP() - attackDamage);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Attack - attack damage is equal to 70% of its");
+        System.out.println("Ability - It could cast a spell on undead which gets the 10% of their HP and add it to its HP");
+        if (super.getHP() > 0) {
+            System.out.print("Choose attack or ability: ");
+            String chooseAttackOrAbility = input.nextLine().toLowerCase();
+            switch (chooseAttackOrAbility.toLowerCase()) {
+                case "attack":
+                    int attackDamage = (int) (super.getHP() * 0.70);
+                    enemy.setHp(enemy.getHP() - attackDamage);
+                    System.out.println("Attack damage: " + attackDamage);
+                    break;
+                case "ability":
+                    ability(enemy);
+                    break;
+            }
+        } else {
+            System.out.println("Attack is not available");
+            String chooseAttackOrAbility = input.nextLine().toLowerCase();
+            switch (chooseAttackOrAbility.toLowerCase()) {
+                case "attack":
+                    System.out.println("You can't attack below 50 HP");
+                    break;
+                case "ability":
+                    ability(enemy);
+                    break;
+            }
+        }
 
         if(enemy.getHP() >= 0) {
-            enemy.setIsDead(true);
+            enemy.setIsDead(false);
         }
     }
     public void attackForGhost(Undead enemy) {
