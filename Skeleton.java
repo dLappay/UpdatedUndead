cb19c7bd3bbd9a1a07a0514b606144625385af8c
dLappay <115202489+dLappay@users.noreply.github.com>
2023-10-04 22:41:28 +0800

    Update Skeleton.java

--- Skeleton.java
+++ Skeleton.java
@@ -1,17 +1,29 @@
-public class Skeleton extends Undead {
+import java.util.Scanner;
+
+public class Skeleton extends Zombie {
     static  String s = "Skeleton";
     public Skeleton() {
         super.setName(s);
         super.setHp(super.getHP() - 20);
     }
-    public void attack(Undead enemy) {
-        //attack
-        int attackDamage = (int) (super.getHP() * 0.7);
-        enemy.setHp(attackDamage);
 
-        if(enemy.getHP() >= 0) {
-            enemy.setIsDead(true);
+    public void attack(Undead enemy) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Attack - Attack damage is 70% of its HP");
+        System.out.println("Ability - No ability");
+        if (super.getHP() > 0) {
+            System.out.print("Choose attack: ");
+            String chooseAttackOrAbility = input.nextLine().toLowerCase();
+            if (chooseAttackOrAbility.equals("attack") || chooseAttackOrAbility.equals("Attack")) {
+                int attackDamage = (int) (super.getHP() * 0.7);
+                enemy.setHp(attackDamage);
+                System.out.println("Attack damage: " + attackDamage);
+            }
+        }else {
+            super.setIsDead(true);
         }
+
+
     }
     public void attackForGhost(Undead enemy) {
         int attackDamage = (int) (super.getHP() * 0.7);
